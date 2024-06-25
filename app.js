f334b8e6f129fa5889c74934b9d93b0f83aeb199
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-06-25 09:55:03 -0700

    Updated Product Page

--- app.js
+++ app.js
@@ -5,22 +5,40 @@ let myFetch = fetch('https://jsonplaceholder.typicode.com/todos').then((data) =>
         let mainDiv = document.getElementById('show_products');
 
         for(let i = 0; i < data.length; i++) {
-            console.log(data[i].title);
+            // console.log(data[i].title);
             // console.log(data , "Data");
 
             let elementCreate = document.createElement("div");
+
             elementCreate.innerHTML = 
             `<p>Product name : <strong>${data[i].title}</strong></p>
-            <p>User Id : <strong>${data[i].id}</strong></p>
+            <p>Product Id : <strong>${data[i].id}</strong></p>
             <p>Quantity : <strong>${data[i].userId}</strong></p>
             <p>Available : <strong>${data[i].completed}</strong></p>`;
 
-            if(data[i].completed === false) {
-                elementCreate.classList.add('false')
-            }
-
             elementCreate.classList.add('border');
             mainDiv.appendChild(elementCreate);
+
+
+            let innerDiv = document.createElement('div');
+            let innerDivText = document.createTextNode('150 X 150');
+            innerDiv.appendChild(innerDivText);
+            innerDiv.classList.add('innerDiv');
+            elementCreate.appendChild(innerDiv);
+
+
+            let addBtn = document.createElement('button');
+            let btnText = document.createTextNode('Add to Card');
+            addBtn.appendChild(btnText);
+
+            addBtn.classList.add('btn');
+
+            elementCreate.appendChild(addBtn);
+
+            if(data[i].completed === false) {
+                elementCreate.classList.add('false');
+                addBtn.classList.add('btn_disabled');
+            }
         }
     })
     .catch((err) => {
